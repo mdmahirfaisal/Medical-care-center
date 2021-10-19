140438516944bd29f3adae14f5f21191d5435da3
unknown <rjmahir.faisal@gmail.com>
2021-10-19 23:24:00 +0600

    footer style added

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -8,11 +8,11 @@ const Footer = () => {
         <div className="footer-container">
             <div className="container pt-5">
                 <div className="row">
-                    <div className="col-12 col-md-5 text-start">
-                        <img className="footer-logo" src="https://i.ibb.co/JqxhFv4/sammy-williams-38-Un6-Oi5be-E-unsplash.png" alt="Hot Onion" />
+                    <div className="col-12 col-md-6 text-start">
+                        <img className="footer-logo" src="https://i.ibb.co/JqxhFv4/sammy-williams-38-Un6-Oi5be-E-unsplash.png" alt="hospital" />
                     </div>
-                    <div className="col-12 col-md-7 d-flex text-start text-light">
-                        <div className=" me-2">
+                    <div className="col-12 col-md-6 d-flex text-start text-light">
+                        <div className="">
                             <p>About Online food</p>
                             <p>Read our blog</p>
                             <p>Sign up delivery</p>
@@ -24,13 +24,16 @@ const Footer = () => {
                             <p>View all cities </p>
                             <p>Restaurants near me </p>
                         </div>
-                        <div className="text-light">
-                            <Link to="/login"><button className="btn btn-outline-danger">Sign up to join </button></Link>
 
-                        </div>
                     </div>
 
                 </div>
+            </div>
+            <div className="d-flex container mt-2">
+                <Link ><button className="btn btn-danger me-3 fs-6"><i className="fab fa-google"></i></button></Link>
+                <Link ><button className="btn btn-danger me-3 fs-6"><i className="fab fa-facebook"></i></button></Link> <br />
+                <Link ><button className="btn btn-danger me-3 fs-6"><i className="fab fa-twitter-square"></i></button></Link>
+                <Link ><button className="btn btn-danger me-3 fs-6"><i className="fab fa-youtube-square"></i></button></Link>
             </div>
 
             <div className=" container d-flex align-items-center">
